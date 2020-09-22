929416c33101c0c58e897b33a1506db362408221
sumsar01 <skriver@live.dk>
2020-09-22 11:58:43 +0200

    Create Titanic_main.py
    Creating main file of the project. Read train and test data.

---
+++ Titanic_main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+#Read the data
+X_train = pd.read_csv('./Data/train.csv')
+X_test = pd.read_csv('./Data/test.csv')
+y_train = X_train.Survived
+X_train.drop(['Survived'], axis=1, inplace=True)
+
+print(y_train.head())
