0382a63f30f82654ebc561db1fbb0722c9b95ba2
sumsar01 <skriver@live.dk>
2020-09-23 16:12:50 +0200

    Update Titanic_main.py
    Cleaning missing values in Titanic data
    
    Drop the single missing fare
    Backfill Embarked
    Age given after average age of title

--- Titanic_main.py
+++ Titanic_main.py
@@ -1,9 +1,42 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as plt
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import rcParams
+
+
 
 #Read the data
 X_train = pd.read_csv('./Data/train.csv')
 X_test = pd.read_csv('./Data/test.csv')
-y_train = X_train.Survived
-X_train.drop(['Survived'], axis=1, inplace=True)
+
+#fusing data to make a larger data set
+data = pd.concat([X_train, X_test], axis=0, sort=True)
 
 
+#Preparing data for use
+data.drop(['Cabin'], axis=1)
+data.Embarked.fillna(method = 'backfill')
+data.Fare.dropna()
+
+#Imputing missing Age
+data['Title'] = data['Name'].str.extract('([A-Za-z]+)\.', expand=True)
+data['Title'].value_counts()
+#Replacing rare titles
+mapping = {'Mlle': 'Miss', 'Major': 'Mr', 'Col': 'Mr', 'Sir': 'Mr',
+           'Don': 'Mr', 'Mme': 'Mrs', 'Jonkheer': 'Mr', 'Lady': 'Mrs',
+           'Capt': 'Mr', 'Countess': 'Mrs', 'Ms': 'Miss', 'Dona': 'Mrs'}
+data.replace({'Title': mapping}, inplace=True)
+
+#median age of title holders
+title_median_age = dict(data.groupby('Title')['Age'].median())
+#average ages
+data['age_med'] = data['Title'].apply(lambda x: title_median_age[x])
+
+#replacing missing ages
+data['Age'].fillna(data['age_med'], inplace=True)
+del data['age_med']
+
+
+print(data['Title'].value_counts())
