dd3697725c76c61e028284ae64600265fff1cbc2
sumsar01 <skriver@live.dk>
2020-09-28 17:17:26 +0200

    Added model
    added Random Forest Classifier model, fitted and made predictions on the validation data.

--- Titanic_main.py
+++ Titanic_main.py
@@ -5,6 +5,9 @@ import matplotlib as plt
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import rcParams
 from  Titanic_data_cleaner import Titanic_data_cleaner
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 rcParams['figure.figsize'] = 10,8
 sns.set(style='whitegrid', palette='bright',
@@ -37,4 +40,50 @@ data = Titanic_data_cleaner(data)
 #Splitting data up again
 train_data = data[pd.notnull(data['Survived'])]
 X_test = data[pd.isnull(data['Survived'])].drop(['Survived'], axis=1)
+
+#splitting training and validation data
+X_train, X_val, y_train, y_val = train_test_split(
+    train_data.drop(['Survived'], axis=1),
+    train_data['Survived'],
+    test_size=0.2, random_state=42)
+
+"""
+#testing shape of new data
+for i in [X_train, X_val, X_test]:
+    print(i.shape)
+"""
+
+#First model
+model = RandomForestClassifier(random_state=0)
+
+#fitting to model
+model.fit(X_train, y_train)
+
+#making predictions on validation data
+preds = model.predict(X_val)
+
+
+
+print(accuracy_score(y_val, preds))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
