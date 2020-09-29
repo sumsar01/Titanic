cf91a9c955bbba3052a80ce9f18024c241de67d3
sumsar01 <skriver@live.dk>
2020-09-29 15:55:01 +0200

    Optimized parameters
    Optimized number of leafs, max_features and bootstrap.

--- Titanic_main.py
+++ Titanic_main.py
@@ -88,9 +88,51 @@ grid_result = grid.fit(X_train, y_train)
 
 print("\nThe best result was with")
 print(grid_result.best_params_)
-print("and had a precision of %f%%, this is a improvement of %f%%"
+print("and had a precision of %f%%, this is a improvement of %f%%\n"
       %(grid_result.best_score_, grid_result.best_score_-cross_val.mean()))
 
+first_opt = grid_result.best_score_
+
+#Now we optimize leaf size
+leaf_samples = [1, 2, 3, 4, 5, 6]
+param_grid = dict(min_samples_leaf=leaf_samples)
+
+model = grid_result.best_estimator_
+ 
+#grid search
+grid = GridSearchCV(estimator=model,
+                    param_grid=param_grid,
+                    cv=3,
+                    verbose=2,
+                    n_jobs=-1)
+
+grid_result = grid.fit(X_train, y_train)
+
+print("\nThe best result was with")
+print(grid_result.best_params_)
+print("and had a precision of %f%%, this is a improvement of %f%%\n"
+      %(grid_result.best_score_, grid_result.best_score_-first_opt))
+
+second_opt = grid_result.best_score_
+
+max_features = [5, 8, 12, None]
+bootstrap = [True, False]
+param_grid = dict(max_features=max_features,bootstrap=bootstrap)
+
+model = grid_result.best_estimator_
+
+grid = GridSearchCV(estimator=model,
+                    param_grid=param_grid,
+                    cv=3,
+                    verbose=2,
+                    n_jobs=-1)
+
+grid_result = grid.fit(X_train, y_train)
+
+print("\nThe best result was with")
+print(grid_result.best_params_)
+print("and had a precision of %f%%, this is a improvement of %f%%\n"
+      %(grid_result.best_score_, grid_result.best_score_-second_opt))
 
 
 
